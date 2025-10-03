19754eac2417fff4bdd8824c218cfd0e2e9defbc
billbillbilly <xiaohaoy111@gmail.com>
2025-10-03 01:45:36 -0400

    update

--- R/ggtern-constructor.R
+++ R/ggtern-constructor.R
@@ -1,6 +1,6 @@
 #' ggtern Constructor
-#' 
-#' Plots in \code{ggtern} are instigated via the default constructor: \code{ggtern(...)}, which is essentially a convenience wrapper for the following: 
+#'
+#' Plots in \code{ggtern} are instigated via the default constructor: \code{ggtern(...)}, which is essentially a convenience wrapper for the following:
 #' \code{ggplot{...} + coord_tern()}, indeed, if one wishes to use \code{ggplot{...} + coord_tern()} then this is quite satisfactory.
 #' @inheritParams ggplot2::ggplot
 #' @param ... additional arguments passed through to \code{\link{ggplot}}
@@ -8,12 +8,10 @@
 #' @return \code{ggtern(...)} returns an object of class \code{ggplot}.
 #' @seealso For an introduction to the \code{ggtern} package, (including many examples), click \link[=ggtern-package]{HERE}.
 #' @author Nicholas Hamilton
-#' @examples 
+#' @examples
 #' ggtern(data=data.frame(x=1,y=1,z=1),aes(x,y,z)) + geom_point()
 #' @export
-ggtern <- function(data=NULL,mapping=aes(),...,environment=parent.frame()){
-  ## Suppress the warning notice of new coordinate system
-  suppressMessages({ 
-    ggplot(data = data, mapping = mapping, environment = environment, ... ) + coord_tern()  
-  })
+ggtern <- function(data=NULL,mapping=ggplot2::aes(),...,environment=parent.frame()){
+  p <- ggplot2::ggplot(data = data, mapping = mapping, ..., environment = environment) + coord_tern()
+  return(p)
 }
